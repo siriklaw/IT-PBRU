93f153a8b2f318cffcafd23ff8720dcaaa22a8cd
siriklaw <sirkklaw_yoocharoen@hotmail.com>
2016-06-06 16:47:21 +0700

    design signin/up success

--- app/src/main/java/pbru/siriklaw/itpbru/MainActivity.java
+++ app/src/main/java/pbru/siriklaw/itpbru/MainActivity.java
@@ -1,7 +1,9 @@
 package pbru.siriklaw.itpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } // Main Method จะมี "  เสมอ
+        // Shif + command + enter
+    public void clicksignUpMain(View view ) {
+        startActivity(new Intent(MainActivity.this,signUpActivity.class));
     }
-}
+}   // Main Class นี่คือ คลาสหลัก
