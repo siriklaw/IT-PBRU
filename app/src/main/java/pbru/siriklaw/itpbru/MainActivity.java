9dcfbb2320117a28f84d0f63166db86f7f25eec1
siriklaw <sirkklaw_yoocharoen@hotmail.com>
2016-06-08 10:22:55 +0700

    SingIn ID/password and check space but not call sqlite

--- app/src/main/java/pbru/siriklaw/itpbru/MainActivity.java
+++ app/src/main/java/pbru/siriklaw/itpbru/MainActivity.java
@@ -9,20 +9,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private MyManage mymanage;
     private static final String urlJSON = "http://swiftcodingthai.com/pbru2/get_user_master.php";
+    private EditText userEditText, passwordEditText;//ตัวแปร
+    private String userString, passwordString;//ตัวแปรรับค่า
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
 
         mymanage = new MyManage(this);
 
@@ -36,6 +46,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     } // Main Method
+
+    public void clickSingIN(View view){
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //check space
+        if(userString.equals("") || passwordString.equals("")){
+            MyAlert myAlert = new MyAlert();
+            myAlert.mydialog(this, "Move Space", "Please fill all every blank");
+
+        }else {
+            checkUserAnPassword();
+        }
+
+
+    }//Click SignIN
+
+    private void checkUserAnPassword() {
+
+    }
+
 
     private void mySynJSON() {
         ConnecUserTABLE connecUserTABLE = new ConnecUserTABLE(this);
@@ -87,6 +119,29 @@ public class MainActivity extends AppCompatActivity {
 
                 progressDialog.dismiss();
                 Log.d("7June", "JSON ==>" + s);
+
+                JSONArray jsonArray = new JSONArray(s);//จองฐานข้อมูล
+
+                String[] idString = new String[jsonArray.length()];
+                String[] nameString = new String[jsonArray.length()];
+                String[] surnameString = new String[jsonArray.length()];
+                String[] userString = new String[jsonArray.length()];
+                String[] passwordString = new String[jsonArray.length()];
+
+                for (int i=0; i<jsonArray.length();i++){
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);//ตัวบ่งชี้
+
+                    idString[i] = jsonObject.getString("id");//อ้างจาก SQL ID
+                    nameString[i] = jsonObject.getString(MyManage.colum_name);
+                    surnameString[i] = jsonObject.getString(MyManage.colum_surname);
+                    userString[i] = jsonObject.getString(MyManage.colum_user);
+                    passwordString[i] = jsonObject.getString(MyManage.colum_password);
+
+                    mymanage.addNewUser(idString[i], nameString[i], surnameString[i], userString[i],
+                            passwordString[i]);//loop string ตำแหน้งที่ I
+
+                }//loop for
 
             } catch (Exception e){
                 e.printStackTrace();
